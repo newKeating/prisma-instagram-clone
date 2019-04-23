f62bc103c1e2f39d2d5ab17c32711118f1a73c2c
Peter Kim <kroits09@gmail.com>
2019-04-23 19:30:13 +0900

    Add Authentication

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,11 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+// import dotenv from "dotenv";
+// import path from "path";
+// dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
 import nodemailer from "nodemailer";
 import sgTransport from "nodemailer-sendgrid-transport";
+import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
@@ -26,7 +28,9 @@ export const sendSecretMail = (address, secret) => {
     from: "peter@prismagram.com",
     to: address,
     subject: "Login Secret for Prismagram 🔒",
-    html: `Hello! Your login secret is ${secret}.<br/>Copy paste on the app/website to log in`
+    html: `Hello! Your login secret is <strong>${secret}</strong>.<br/>Copy paste on the app/website to log in`
   };
   return sendMail(email);
 };
+
+export const generateToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
